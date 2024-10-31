0f089ac15d59f60797d468a4bea6479a4ece786f
bensatlantik <bensantora@proton.me>
2024-10-30 21:21:22 -0400

    Update

--- target/debug/deps/uuid_base64-1245ff841b440f74.d
+++
@@ -1,5 +0,0 @@
-/home/ben/uuid_base64/target/debug/deps/uuid_base64-1245ff841b440f74: src/lib.rs
-
-/home/ben/uuid_base64/target/debug/deps/uuid_base64-1245ff841b440f74.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/uuid_base64-e6182ecdc977a93b.d
+++
@@ -1,7 +0,0 @@
-/home/ben/uuid_base64/target/debug/deps/libuuid_base64-e6182ecdc977a93b.rmeta: src/lib.rs
-
-/home/ben/uuid_base64/target/debug/deps/libuuid_base64-e6182ecdc977a93b.rlib: src/lib.rs
-
-/home/ben/uuid_base64/target/debug/deps/uuid_base64-e6182ecdc977a93b.d: src/lib.rs
-
-src/lib.rs:
